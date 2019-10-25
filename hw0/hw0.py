37a520f6b893d173b3563da9779c15fa75a63b71
cvantie1 <cvantie1@linux6.gl.umbc.edu>
2019-10-25 12:36:29 -0400

    hw0

---
+++ hw0/hw0.py
@@ -0,0 +1,11 @@
+# File: hw0.py
+# Author: Caroline Vantiem
+# Date: 9/10/2017
+# Section: 3
+# E-mail: cvantie1@umbc.edu
+# Description: This file is to help students practice
+# using the submission system on the GL
+# servers for CMSC 201.
+
+print("This is going to be a great semester!")
+
